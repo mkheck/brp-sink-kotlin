b8b3a72404749c1fba12a8f1b8c1ef3dbc4a49fc
Mark Heckler <mark.heckler@gmail.com>
2019-12-01 20:06:43 -0500

    Testing what works properly, what doesn't

--- src/main/kotlin/com/thehecklers/brpsinkkotlin/BrpSinkKotlinApplication.kt
+++ src/main/kotlin/com/thehecklers/brpsinkkotlin/BrpSinkKotlinApplication.kt
@@ -7,30 +7,46 @@ import org.springframework.cloud.stream.annotation.StreamListener
 import org.springframework.cloud.stream.messaging.Sink
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import reactor.core.publisher.Flux
 import java.util.function.Consumer
 
 @SpringBootApplication
 class BrpSinkKotlinApplication
 
 fun main(args: Array<String>) {
-	runApplication<BrpSinkKotlinApplication>(*args)
+    runApplication<BrpSinkKotlinApplication>(*args)
 }
 
-//@Configuration
-@EnableBinding(Sink::class)
+@Configuration
+//@EnableBinding(Sink::class)
 class FlightAttendant {
-//	@Bean
-////	fun seatPassenger(): (FlyingPassenger) -> Unit = { println(it) }
-//	fun seatPassenger(): Consumer<FlyingPassenger> =
-//		Consumer<FlyingPassenger> { println(it) }
+    /*
+    //MH: This does NOT work (but it should)
+    @Bean
+    fun seatPassenger(): Consumer<Flux<FlyingPassenger>> =
+        Consumer {
+            it.map {
+                println(it)
+            }
+        }
+    */
 
-	@StreamListener(Sink.INPUT)
+    //MH: This does work, but it isn't dealing with a Flux
+    @Bean
+    fun seatPassenger(): Consumer<FlyingPassenger> =
+        Consumer {
+            println(it)
+        }
+
+    /*
+    @StreamListener(Sink.INPUT)
 	fun seatPassenger(flyingPassenger: FlyingPassenger) = println(flyingPassenger)
+    */
 }
 
 data class FlyingPassenger(val id: String, val name: String, val state: State) {
-	enum class State {
-		VALUED_PASSENGER,
-		PREMIUM_PASSENGER
-	}
+    enum class State {
+        VALUED_PASSENGER,
+        PREMIUM_PASSENGER
+    }
 }
